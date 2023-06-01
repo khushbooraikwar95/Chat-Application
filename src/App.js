5e637d5c0235976d6ca0ff4ef36940aeb5e5ed83
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2023-06-01 21:32:27 +0530

    style- style added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { ChatEngine } from "react-chat-engine";
 
 function App() {
-  return <div className="App">Chit-chat</div>;
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="
+  b8b7221a-02a3-4cf2-8413-96894de68daf"
+      userName=""
+      userSecret=""
+    />
+  );
 }
 
 export default App;
