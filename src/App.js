3a1708e0c2f235cd56488acfb6c3690988ed0b79
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2023-06-08 13:36:34 +0530

    feat - MyMessage and TheirMessage functionality implementation

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <ChatEngine
       height="100vh"
       projectID="b8b7221a-02a3-4cf2-8413-96894de68daf"
-      userName="Khushboo"
-      userSecret="1234"
+      userName="Vicky"
+      userSecret="4321"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
