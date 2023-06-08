b42697eee867a4c1c990f25eb093410b8eff0758
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2023-06-08 16:30:45 +0530

    feat- create Login form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
 function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
       projectID="b8b7221a-02a3-4cf2-8413-96894de68daf"
-      userName="Vicky"
-      userSecret="4321"
+      userName="Khushboo"
+      userSecret="1234"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
